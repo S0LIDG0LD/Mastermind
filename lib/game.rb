f75305533b9f9894a1c7ea3467dee272c50125c2
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-08-26 17:44:06 +0100

    Added check(guess) method to the Game class Updated the display_board to print the board with the history of every round FIXME - There's a bug that changes the order of the entered words when the words are correct and in the correct place

--- lib/game.rb
+++ lib/game.rb
@@ -15,18 +15,15 @@ class Game
     @solution = create_random_solution
     puts "The secret combination to guess is: #{@solution}"
     @rounds_played = []
-    # @current_player_id = 0
-    # @players = [player1.new(self, 'X'), player2.new(self, 'O')]
-    puts @rounds_played
     display_board
   end
 
   def game_title
     puts
-    puts "#{@current_player} has 12 rounds to guess a combination of #{SOLUTION_SIZE} of these colours:"
+    puts "#{@player} has 12 rounds to guess a combination of #{SOLUTION_SIZE} of these colours. Colours can be repeated!"
     puts COLOURS.join(', ')
     puts
-    puts "The Mastermind is #{@current_mastermind}, BEWARE!"
+    puts "The Mastermind is #{@mastermind}, BEWARE!"
     puts
   end
 
@@ -47,26 +44,71 @@ class Game
   def can_play_round?
     return false if current_round > NUMBER_OF_ROUNDS
 
-    puts 'can play round!'
-
     guess = @player.make_guess!
     puts "guess: #{guess}, solution: #{@solution}"
     return false if guessed?(guess)
 
     # @board_matrix[position[0] - 1][position[1] - 1] = player.symbol
-    display_board
+    # display_board
     true
   end
 
   def guessed?(guess)
+    guessed = false
+    last_round = []
+    puts
     if guess == @solution
-      puts 'GUESSED!!!'
-      true
+      puts "Congratulations #{@player}! #{@player} guessed correctly in #{@rounds_played.size + 1} rounds"
+      # puts @rounds_played
+      guessed = true
+      guess.split.each do |item|
+        word_and_result = []
+        word_and_result.push(item)
+        word_and_result.push('X')
+        last_round.push(word_and_result)
+      end
     else
-      puts 'NOT GUESSED!!!'
-      check(guess)
+      puts "#{@player} has not guessed correctly. #{@player} has #{12 - @rounds_played.size - 1} more rounds to guess the solution"
+      last_round = check(guess)
     end
+    # puts last_round
+    @rounds_played.push(last_round)
+    display_board
+    guessed
   end
+
+  def check(guess)
+    round = []
+    checked_solution = @solution.split
+    checked_guess = guess.split
+    index_size = checked_guess.size
+    index = 0
+    while index < index_size
+      word_and_result = []
+      if checked_guess[index] == checked_solution[index]
+        word_and_result.push(checked_guess[index])
+        word_and_result.push('X')
+        checked_guess.delete_at(index)
+        checked_solution.delete_at(index)
+        index_size -= 1
+        round.push(word_and_result)
+      end
+      index += 1
+    end
+    checked_guess.each do |item|
+      word_and_result = []
+      word_and_result.push(item)
+      if checked_solution.include?(item)
+        word_and_result.push('O')
+        checked_solution.delete(item)
+      else
+        word_and_result.push('-')
+      end
+      round.push(word_and_result)
+    end
+    round
+  end
+
   # def current_player
   #   @players[@current_player_id]
   # end
@@ -93,22 +135,33 @@ class Game
 
   def display_border_line(size = 'long')
     if size == 'short'
-      puts '          +------------------------------------+'
+      puts '              +---------------------------------+'
     else
-      puts '+---------+------------------------------------+'
+      puts '+-------------+---------------------------------+'
     end
   end
 
   def display_board
+    puts
     display_border_line('short')
-    puts '          |    ?        ?        ?        ?    |'
+    puts '              |    ?       ?       ?       ?    |'
     display_border_line
-    @rounds_played.each_with_index do |row, index|
-      puts "| #{index} | #{row.join('  ')}" if index.positive?
+    @rounds_played.reverse.each_with_index do |words, index|
+      print "| #{@rounds_played.size - index} | "
+      result = ''
+      adjusted_guess = ''
+      words.each do |word|
+        result += "#{word[1]} "
+        adjusted_guess += word[0].center(8)
+      end
+      puts "#{result}| #{adjusted_guess}|"
       display_border_line
     end
     puts
   end
+
+  # def adjust_string(word, length)
+  # end
 
   # def player_won?(symbol)
   #   WINNING_CONDITIONS.each do |condition|
--- lib/human_player.rb
+++ lib/human_player.rb
@@ -4,7 +4,7 @@
 class HumanPlayer < Player
   def make_guess!
     round = "This is round #{@game.current_round} of #{@game.number_of_rounds}. "
-    print "#{round}Select a #{@game.number_of_colours} colour combiation: "
+    print "#{round}Choose #{@game.number_of_colours} colours: "
     gets.chomp.split(',').join(' ')
     # @game.display_board
   end
